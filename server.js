053264490c4b36fa64cfd360b5ca422f13639eaf
Anders Kromann Nielsen <anders@kromann.me>
2016-06-07 12:12:52 +0200

    Basic functionality

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var http = require("http");
+var url = require("url");
 
-function start(){
+function start(route){
   // Handles requests to the server
   function onRequest(request, response){
-    console.log("Request received.");
+    // Get the pathname from the url
+    var pathname = url.parse(request.url).pathname;
+
+    console.log("Request for " + pathname + " received.");
+    route(pathname);
+
+    // Create the response.
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World!");
     response.end();
