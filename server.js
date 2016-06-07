c45ab4a4c2637f813409a86ea50c4e296290743c
Anders Kromann Nielsen <anders@kromann.me>
2016-06-07 12:33:53 +0200

    Fixing response handling

--- server.js
+++ server.js
@@ -8,12 +8,8 @@ function start(route, handle){
     var pathname = url.parse(request.url).pathname;
 
     console.log("Request for " + pathname + " received.");
-    route(handle, pathname);
 
-    // Create the response.
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World!");
-    response.end();
+    route(handle, pathname, response);
   }
 
   // Starts the server
