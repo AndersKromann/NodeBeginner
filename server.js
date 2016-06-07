8a30a49dd574c03ab747004430a8157283540cb1
Anders Kromann Nielsen <anders@kromann.me>
2016-06-07 12:57:13 +0200

    Added basic post functionality

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ var url = require("url");
 function start(route, handle){
   // Handles requests to the server
   function onRequest(request, response){
+    var postData = "";
     // Get the pathname from the url
     var pathname = url.parse(request.url).pathname;
-
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    // Adds a listener for new postdata coming in.
+    request.addListener("data", function(postDataChunk){
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" + postDataChunk + "'.");
+    });
+
+    // Adds a listener to handle routing of the postdata only when done.
+    request.addListener("end", function(){
+      route(handle, pathname, response, postData);
+    });
   }
 
   // Starts the server
